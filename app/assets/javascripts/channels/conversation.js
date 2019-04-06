300e3dae4aa6713edce68e7265e16de4b952293d
Armand <armand.j.rodriguez@gmail.com>
2019-04-05 23:34:14 -0700

    final fix on messages stylings

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -9,10 +9,22 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
     console.log('recieved');
     console.log(data);
     $(".messages-section").append(data.message);
-    $(".alert.alert-info").show().delay(2000).fadeOut('slow');
+    // console.log( $(data.message).attr("data-message-user-id") );
+    // if ( parseInt($(data.message).data("message-user-id")) === $('[data-user-id]').data('user-id') ) {
+    //   console.log('add class');
+    // }
+    var bubbles = $(".bubble");
+    if ( parseInt($(".bubble").last().data("message-user-id")) === $('[data-user-id]').data('user-id') ) {
+      $(".bubble").last().addClass("bubble-primary");
+    } else {
+      $(".bubble").last().addClass("bubble-secondary");
+    }
+
+
     $("#message_body").val("");
     var messages = $("#chatbox");
     messages.scrollTop(messages[0].scrollHeight);
+
   },
   listen_to_messages: function(){
     console.log('listen_to_messages');
--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,1 +1,11 @@
-
+$(document).on('turbolinks:load', function() {
+  $(".bubble").each( function() {
+      if ( parseInt($(this).data("message-user2-id")) === $('[data-user-id]').data('user-id') ){
+        $(this).addClass("bubble-primary");
+        console.log($(this));
+      } else {
+        $(this).addClass("bubble-secondary");
+      }
+    }
+  );
+});
